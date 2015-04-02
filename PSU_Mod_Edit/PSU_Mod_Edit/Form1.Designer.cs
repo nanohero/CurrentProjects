75e911ee9f579c6d4e7f7c9ae8d8754d67a938b2
nanohero <coreyshellenberger@hotmail.com>
2015-04-01 22:10:15 -0700

    first change

---
+++ PSU_Mod_Edit/PSU_Mod_Edit/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PSU_Mod_Edit
+{
+    partial class frmTitle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.namLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(55, 299);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(108, 23);
+            this.name.TabIndex = 0;
+            this.name.Text = "Search";
+            this.name.UseVisualStyleBackColor = true;
+            this.name.Click += new System.EventHandler(this.name_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 299);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "illuminus coat"});
+            this.comboBox1.Location = new System.Drawing.Point(87, 184);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // namLbl
+            // 
+            this.namLbl.AutoSize = true;
+            this.namLbl.Location = new System.Drawing.Point(39, 36);
+            this.namLbl.Name = "namLbl";
+            this.namLbl.Size = new System.Drawing.Size(90, 13);
+            this.namLbl.TabIndex = 3;
+            this.namLbl.Text = "Character Name: ";
+            // 
+            // frmTitle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 377);
+            this.Controls.Add(this.namLbl);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.name);
+            this.Name = "frmTitle";
+            this.Text = "Psu Mod Edit";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button name;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label namLbl;
+    }
+}
+
---
+++ PSU_Mod_Edit/PSU_Mod_Edit/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace PSU_Mod_Edit
+{
+    public partial class frmTitle : Form
+    {
+        public frmTitle()
+        {
+            InitializeComponent();
+        }
+        //import win api functions
+        const int PROCES_WM_READ = 0x0010;
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAcess, bool bInheritHandle, int dwProcessId);
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAdress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+        
+
+      
+        private void name_Click(object sender, EventArgs e)
+        {
+           
+                //get psu process
+                Process psu = Process.GetProcessesByName("PsuIlluminus_Clementine")[0];
+                IntPtr Handle = OpenProcess(PROCES_WM_READ, false, psu.Id);
+                // get number of bytes
+                int bytesRead = 0;
+                byte[] buffer = new byte[24];
+                //reads memory and get's name of player character
+                ReadProcessMemory((int)Handle, 0x00B6CA02, buffer, buffer.Length, ref bytesRead);
+                //should output charter name in messeage box and label
+                //it does not
+                namLbl.Text =Encoding.Unicode.GetString(buffer);
+                MessageBox.Show(Encoding.Unicode.GetString(buffer));
+                MessageBox.Show("test"); //use to test if messeage boxes at all works.This one works.
+                
+         
+
+            
+          
+            
+        }
+
+    
+      
+    }
+}
