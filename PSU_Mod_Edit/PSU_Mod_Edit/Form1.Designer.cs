04a7057e2666c8ec619aaa13f789880983bf1fc4
nanohero <coreyshellenberger@hotmail.com>
2015-04-16 10:59:32 -0700

    suff is strating to work

--- PSU_Mod_Edit/PSU_Mod_Edit/Form1.Designer.cs
+++ PSU_Mod_Edit/PSU_Mod_Edit/Form1.Designer.cs
@@ -32,7 +32,7 @@
             this.name = new System.Windows.Forms.Button();
             this.cboClothes = new System.Windows.Forms.ComboBox();
             this.namLbl = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
             this.btnEdit = new System.Windows.Forms.Button();
             this.lblParts = new System.Windows.Forms.Label();
             this.cboParts = new System.Windows.Forms.ComboBox();
@@ -43,6 +43,15 @@
             this.lblColor = new System.Windows.Forms.Label();
             this.cboColor = new System.Windows.Forms.ComboBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTop = new System.Windows.Forms.TextBox();
+            this.txtBottom = new System.Windows.Forms.TextBox();
+            this.txtShoes = new System.Windows.Forms.TextBox();
+            this.chkMaleParts = new System.Windows.Forms.CheckBox();
+            this.chkFemaleParts = new System.Windows.Forms.CheckBox();
+            this.chkAotiColors = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // name
@@ -50,15 +59,13 @@
             resources.ApplyResources(this.name, "name");
             this.name.Name = "name";
             this.name.UseVisualStyleBackColor = true;
-          
             // 
             // cboClothes
             // 
             this.cboClothes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboClothes.FormattingEnabled = true;
             this.cboClothes.Items.AddRange(new object[] {
-            resources.GetString("cboClothes.Items"),
-            resources.GetString("cboClothes.Items1")});
+            resources.GetString("cboClothes.Items")});
             resources.ApplyResources(this.cboClothes, "cboClothes");
             this.cboClothes.Name = "cboClothes";
             this.cboClothes.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
@@ -68,12 +75,12 @@
             resources.ApplyResources(this.namLbl, "namLbl");
             this.namLbl.Name = "namLbl";
             // 
-            // button1
+            // btnStart
             // 
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            resources.ApplyResources(this.btnStart, "btnStart");
+            this.btnStart.Name = "btnStart";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnEdit
             // 
@@ -114,12 +121,14 @@
             resources.ApplyResources(this.chkMale, "chkMale");
             this.chkMale.Name = "chkMale";
             this.chkMale.UseVisualStyleBackColor = true;
+            this.chkMale.CheckedChanged += new System.EventHandler(this.chkMale_CheckedChanged);
             // 
             // chkFemale
             // 
             resources.ApplyResources(this.chkFemale, "chkFemale");
             this.chkFemale.Name = "chkFemale";
             this.chkFemale.UseVisualStyleBackColor = true;
+            this.chkFemale.CheckedChanged += new System.EventHandler(this.chkFemale_CheckedChanged);
             // 
             // lblColor
             // 
@@ -132,14 +141,73 @@
             this.cboColor.FormattingEnabled = true;
             resources.ApplyResources(this.cboColor, "cboColor");
             this.cboColor.Name = "cboColor";
-           
             this.cboColor.MouseClick += new System.Windows.Forms.MouseEventHandler(this.cboColor_MouseClick);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // txtTop
+            // 
+            resources.ApplyResources(this.txtTop, "txtTop");
+            this.txtTop.Name = "txtTop";
+            // 
+            // txtBottom
+            // 
+            resources.ApplyResources(this.txtBottom, "txtBottom");
+            this.txtBottom.Name = "txtBottom";
+            // 
+            // txtShoes
+            // 
+            resources.ApplyResources(this.txtShoes, "txtShoes");
+            this.txtShoes.Name = "txtShoes";
+            // 
+            // chkMaleParts
+            // 
+            resources.ApplyResources(this.chkMaleParts, "chkMaleParts");
+            this.chkMaleParts.Name = "chkMaleParts";
+            this.chkMaleParts.UseVisualStyleBackColor = true;
+            this.chkMaleParts.CheckedChanged += new System.EventHandler(this.chkMaleParts_CheckedChanged);
+            // 
+            // chkFemaleParts
+            // 
+            resources.ApplyResources(this.chkFemaleParts, "chkFemaleParts");
+            this.chkFemaleParts.Name = "chkFemaleParts";
+            this.chkFemaleParts.UseVisualStyleBackColor = true;
+            this.chkFemaleParts.CheckedChanged += new System.EventHandler(this.chkFemaleParts_CheckedChanged);
+            // 
+            // chkAotiColors
+            // 
+            resources.ApplyResources(this.chkAotiColors, "chkAotiColors");
+            this.chkAotiColors.Name = "chkAotiColors";
+            this.chkAotiColors.UseVisualStyleBackColor = true;
+            this.chkAotiColors.CheckedChanged += new System.EventHandler(this.chkAotiColors_CheckedChanged);
             // 
             // frmTitle
             // 
             this.AcceptButton = this.btnEdit;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.chkAotiColors);
+            this.Controls.Add(this.chkFemaleParts);
+            this.Controls.Add(this.chkMaleParts);
+            this.Controls.Add(this.txtShoes);
+            this.Controls.Add(this.txtBottom);
+            this.Controls.Add(this.txtTop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblColor);
             this.Controls.Add(this.cboColor);
             this.Controls.Add(this.chkFemale);
@@ -149,7 +217,7 @@
             this.Controls.Add(this.cboParts);
             this.Controls.Add(this.lblParts);
             this.Controls.Add(this.btnEdit);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.namLbl);
             this.Controls.Add(this.cboClothes);
             this.Controls.Add(this.name);
@@ -165,7 +233,7 @@
         private System.Windows.Forms.Button name;
         private System.Windows.Forms.ComboBox cboClothes;
         private System.Windows.Forms.Label namLbl;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Label lblParts;
         private System.Windows.Forms.ComboBox cboParts;
@@ -176,6 +244,15 @@
         private System.Windows.Forms.Label lblColor;
         private System.Windows.Forms.ComboBox cboColor;
         private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTop;
+        private System.Windows.Forms.TextBox txtBottom;
+        private System.Windows.Forms.TextBox txtShoes;
+        private System.Windows.Forms.CheckBox chkMaleParts;
+        private System.Windows.Forms.CheckBox chkFemaleParts;
+        private System.Windows.Forms.CheckBox chkAotiColors;
     }
 }
 
--- PSU_Mod_Edit/PSU_Mod_Edit/Form1.cs
+++ PSU_Mod_Edit/PSU_Mod_Edit/Form1.cs
@@ -28,95 +28,250 @@ namespace PSU_Mod_Edit
         [DllImport("kernel32.dll",SetLastError = true)]
         static extern bool WriteProcessMemory(int hProcess, int lpBaseAdress, byte[] lpBuffer, int dwSize,ref int lpNumberOfBytesWritten );
         Process psu = Process.GetProcessesByName("PsuIlluminus_Clementine")[0];
-       
-        int byteValue;
-      int byteValue2;
-     
-
-        
-
+        int byteValue=0;
+        string line;
+     string [] malesClothes=new string[37];
+     string[] malesClothes2 = new string[37];
+     string []femaleClothes=new string[40];
+     string[] maleParts = new string[29];
+     string[] femaleParts = new string[36];
+     string[] aotiColors = new string[35];
         private void button1_Click(object sender, EventArgs e)
         {
-          
-                   
             if (Process.GetProcessesByName("PsuIlluminus_Clementine").Length > 0)
             {
                 MessageBox.Show("psu clem is already running");
-            }          else
+            }
+            else
             {
                 Process.Start(@"C:\SEGA\PHANTASY STAR UNIVERSE Illuminus\PsuIlluminus_Clementine");
-          
 
-           
             }
-            
-            
         }
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (cboClothes.SelectedItem.ToString() == "illuminus coat")
+            if (chkMale.Checked==true)
             {
-                byteValue = 21;
+                ByteValueChangeMale();
             }
-
+            if (chkFemale.Checked==true)
+            {
+                
+            }
+               
         }
+
+        private void ByteValueChangeMale()
+        {
+            if (cboClothes.SelectedItem.ToString() == malesClothes[0])
+            {
+                byteValue = 00;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[1])
+            {
+                byteValue = 0x01;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[2])
+            {
+                byteValue = 0x02;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[3])
+            {
+                byteValue = 0x03;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[4])
+            {
+                byteValue = 0x04;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[5])
+            {
+                byteValue = 0x05;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[6])
+            {
+                byteValue = 0x06;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[7])
+            {
+                byteValue = 0x07;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[8])
+            {
+                byteValue = 0x08;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[9])
+            {
+                byteValue = 0x09;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[10])
+            {
+                byteValue = 0x0A;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[11])
+            {
+                byteValue = 0x0B;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[12])
+            {
+                byteValue = 0x0C;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[13])
+            {
+                byteValue = 0x0D;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[14])
+            {
+                byteValue = 0x0E;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[15])
+            {
+                byteValue = 0x0F;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[16])
+            {
+                byteValue = 0x10;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[17])
+            {
+                byteValue = 0x11;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[18])
+            {
+                byteValue = 0x12;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[19])
+            {
+                byteValue = 0x13;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[20])
+            {
+                byteValue = 0x14;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[21])
+            {
+                byteValue = 0x15;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[22])
+            {
+                byteValue = 0x16;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[23])
+            {
+                byteValue = 0x17;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[24])
+            {
+                byteValue = 0x18;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[25])
+            {
+                byteValue = 0x19;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[26])
+            {
+                byteValue = 0x1A;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[27])
+            {
+                byteValue = 0x1B;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[28])
+            {
+                byteValue = 0x1C;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[29])
+            {
+                byteValue = 0x1D;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[30])
+            {
+                byteValue = 0x1E;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[31])
+            {
+                byteValue = 0x1F;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[32])
+            {
+                byteValue = 0x20;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[33])
+            {
+                byteValue = 0x21;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[34])
+            {
+                byteValue = 0x22;
+            }
+            if (cboClothes.SelectedItem.ToString() == malesClothes[35])
+            {
+                byteValue = 0x23;
+            }               
+        }
+
+       
         private void button2_Click(object sender, EventArgs e)
         {
-            Process psu = Process.GetProcessesByName("PsuIlluminus_Clementine")[0];
+           
             IntPtr Handle = OpenProcess(0x01F0FFF, false, psu.Id);
             int bytesWritten = 0;
-            byte[] buffer = BitConverter.GetBytes(byteValue);
-            byte[] buffer2 = BitConverter.GetBytes(byteValue2); 
-        
-            // get number of bytes
-
-
+            byte[] buffer = new byte[1];
+            buffer[0] =(byte)Convert.ToInt32(byteValue);          
 
             if (cboParts.SelectedItem.ToString() == "top")
             {
-
-                WriteProcessMemory((int)Handle, 0x05C37B05, buffer, buffer.Length, ref bytesWritten);
-           
-               
-            }
-            
+                WriteProcessMemory((int)Handle, 0x057E9B05, buffer, buffer.Length, ref bytesWritten);                       
+            }           
             if (cboParts.SelectedItem.ToString() == "bottoms")
             {
-                byteValue2 = 1;
-                WriteProcessMemory((int)Handle, 0x05C37B09, buffer, buffer.Length, ref bytesWritten);
-                WriteProcessMemory((int)Handle, 0x05C37B0B, buffer2, buffer2.Length, ref bytesWritten);
-                MessageBox.Show("test");
+                WriteProcessMemory((int)Handle, 0x057E9B09, buffer, buffer.Length, ref bytesWritten);                             
             }
-
             if (cboParts.SelectedItem.ToString() == "shoes")
             {
-                byteValue2 = 2;
-                WriteProcessMemory((int)Handle, 0x05C37B0D, buffer, buffer.Length, ref bytesWritten);
-                WriteProcessMemory((int)Handle, 0x05C37B0F, buffer2, buffer2.Length, ref bytesWritten);
-
-            }
-
-            
-        }
-
-      
-
-      
-
+                WriteProcessMemory((int)Handle, 0x057E9B0D, buffer, buffer.Length, ref bytesWritten);                             
+            }          
+        }            
         private void cboColor_MouseClick(object sender, MouseEventArgs e)
         {
-            ColorDialog colorDlg = new ColorDialog();
-    
-            
+            ColorDialog colorDlg = new ColorDialog();                
             if (colorDlg.ShowDialog()==DialogResult.OK)
             {
                 cboColor.BackColor = colorDlg.Color;
             }
         }
-
-   
-
-    
-      
+        private void chkMale_CheckedChanged(object sender, EventArgs e)
+        {
+            AddClothes(@"male_clothes.txt",malesClothes);
+        }
+        private void AddClothes(string fileName,string[] clothesArray)
+        {
+            int i = 0;
+            System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+            while ((line = file.ReadLine()) != null)
+            {
+              clothesArray[i] = line;
+                i++;
+            }
+            file.Close();
+            BindingSource theBindingSource = new BindingSource();
+            theBindingSource.DataSource = clothesArray;
+            cboClothes.DataSource = theBindingSource.DataSource;
+        }
+        private void chkFemale_CheckedChanged(object sender, EventArgs e)
+        {
+            AddClothes(@"female_clothes.txt",femaleClothes);
+        }
+        private void chkMaleParts_CheckedChanged(object sender, EventArgs e)
+        {
+            AddClothes(@"male_parts.txt",maleParts);
+        }
+        private void chkFemaleParts_CheckedChanged(object sender, EventArgs e)
+        {
+            AddClothes(@"female_parts.txt",femaleParts); 
+        }
+        private void chkAotiColors_CheckedChanged(object sender, EventArgs e)
+        {
+            AddClothes(@"aoti_colors.txt",aotiColors);
+        }         
     }
 }
